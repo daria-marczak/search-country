36dd45ef22541103b025ae8cfacf9e34a274b34d
daria marczak <marczak.ria@gmail.com>
2017-11-26 15:08:37 +0100

    added a feature

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 var url = "https://restcountries.eu/rest/v1/name/";
 var countriesList = $("#countries");
 
-$("#search").click(searchCountries);
 
-function searchCountries() {
-    var countryName = $("#country-name").val();
-    if (!countryName.length) countryName = "France";
+$("input[type = text]").on("keydown", function(e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        $("#search").click(searchCountries);
 
-    $.ajax({
-        url: url + countryName,
-        method: "GET",
-        success: showCountriesList
-    });
+        function searchCountries() {
+            var countryName = $("#country-name").val();
+            if (!countryName.length) countryName = "France";
 
-    function showCountriesList(resp) {
-        countriesList.empty();
-	    resp.forEach(function(item){
-	   		$("<h3>").text(item.name).appendTo(countriesList);
-	   		$("<p>").text("Country's capital is " + item.capital).appendTo(countriesList);
-	   		$("<p>").text("The country currency is " + item.currencies).appendTo(countriesList);
-	   		$("<p>").text("People of " + item.name + " speaks " + item.languages).appendTo(countriesList);
-		});
+            $.ajax({
+                url: url + countryName,
+                method: "GET",
+                success: showCountriesList
+            });
+
+            function showCountriesList(resp) {
+                countriesList.empty();
+        	    resp.forEach(function(item){
+        	   		$("<h3>").text(item.name).appendTo(countriesList);
+        	   		$("<p>").text("Country's capital is " + item.capital).appendTo(countriesList);
+        	   		$("<p>").text("The country currency is " + item.currencies).appendTo(countriesList);
+        	   		$("<p>").text("People of " + item.name + " speak " + item.languages).appendTo(countriesList);
+        		});
+            }
+        }
     }
-}
+});
