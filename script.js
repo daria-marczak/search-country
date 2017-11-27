1de264cac740b07011ee9afb848b62f1e2bae1e2
Daria Marczak <31421134+daria-marczak@users.noreply.github.com>
2017-11-27 13:29:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function searchCountries() {
             $("<h3>").text(item.name).appendTo(countriesBox);
             $("<p>").text("Country's capital is " + item.capital).appendTo(countriesBox);
             $("<p>").text("The country currency is " + item.currencies).appendTo(countriesBox);
-            $("<p>").text("People of " + item.name + " speak " + item.languages).appendTo(countriesBox);
+            $("<p>").text("People of " + item.name + " speak " + item.nativeName).appendTo(countriesBox);
         });
     }
 }
